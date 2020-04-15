37ce9def160378c42a8c2a87f05399ee5d83779e
Diamondlord <vladimir.almazov@gmail.com>
2020-04-15 12:02:01 +0300

    update angular material to 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule,
-  MatInputModule, MatProgressBarModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
